4dbcf1660d8d86f2c1476dd990a7b2b1decd8e7f
zavozina <77191758+zavozina01@users.noreply.github.com>
2021-01-12 18:58:24 +0700

    start

---
+++ 42git/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _42git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
